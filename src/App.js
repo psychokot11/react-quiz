c61ccce70b11cfdb37f45547ad75698810c28da7
Zofia Stawarska <z.stawarska@gmail.com>
2024-02-15 12:57:09 +0100

    fetch data and set ready&error state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const initialState = {
+  questions: [],
+  status: "loading"
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready"
+      }
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error"
+      }
+    default:
+      throw new Error("Invalid action type")
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function() {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:5000/questions");
+        const data = await response.json();
+        dispatch({ type: "dataReceived", payload: data })
+      } catch (err) {
+        dispatch({ type: "dataFailed" })
+      }
+    }
+  
+    fetchData();
+  }, [])
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+     <Main>
+      <p>1/15</p>
+      <p>Welcome :)</p>
+     </Main>
     </div>
   );
 }
